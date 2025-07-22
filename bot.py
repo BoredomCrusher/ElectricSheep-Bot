3ec01bb6cfdff7b67ef7cc39f63feca8bcb70a45
BoredomCrusher <Matrix1000atr9@gmail.com>
2025-07-22 14:14:11 -0700

    basic setup and shrug function.

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+intents = commands.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Load Cogs (feature modules)
+initial_extensions = ['cogs.submission_grinder', 'cogs.tracker', 'cogs.exquisite_corpse', 'cogs.shrug']
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has awakened, beep beep I\'m a sheep')
+
+for ext in initial_extensions:
+    bot.load_extension(ext)
+
+bot.run(TOKEN)
---
+++ cogs/shrug.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+class Shrug(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        if "/shrug" in message.content:
+            await message.channel.send("¯\\_(ツ)_/¯")
+
+async def setup(bot):
+    await bot.add_cog(Shrug(bot))
