5df9e83d2c70f58b944dfeb5d23ebd6430b30b8e
BoredomCrusher <Matrix1000atr9@gmail.com>
2025-07-22 18:31:45 -0700

    fixed bug, bot can be active now

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import os
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-intents = commands.Intents.default()
+intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
