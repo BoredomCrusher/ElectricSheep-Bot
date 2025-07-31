d5cd5bde67a04be2c3b054b12165fca3163c0661
BoredomCrusher <Matrix1000atr9@gmail.com>
2025-07-31 13:41:05 -0700

    more testing.

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # Load Cogs (feature modules)
-initial_extensions = ['cogs.submission_grinder', 'cogs.tracker', 'cogs.exquisite_corpse', 'cogs.bot_commands']
+# initial_extensions = ['cogs.submission_grinder', 'cogs.tracker', 'cogs.exquisite_corpse', 'cogs.bot_commands']
 
 @bot.event
 async def on_ready():
@@ -21,8 +21,8 @@ async def on_ready():
 
 # This is supposed to have an await, but not all of the files are done yet,
 # so this is going to be added under main once all of the functions are ready.
-for ext in initial_extensions:
-     bot.load_extension(ext)
+# for ext in initial_extensions:
+#      bot.load_extension(ext)
 
 async def main():
     async with bot:
