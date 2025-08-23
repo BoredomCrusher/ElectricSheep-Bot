d9a5b7c7ee6e14a1866cd777a563601ceed42716
boredomcrusher <Matrix1000atr9@gmail.com>
2025-08-23 16:26:13 -0700

    updates for tracker.

--- cogs/submission_grinder.py
+++ cogs/submission_grinder.py
@@ -93,7 +93,6 @@ class Submission_Grinder(commands.Cog):
     # Runs every 24 hours to not scrape the website too often.
     @tasks.loop(time=datetime.time(hour=23, minute=59, tzinfo=ZoneInfo("America/Los_Angeles")))
     async def daily_fetch_website(self):
-        # regular daily scraping currently removed for testing while relying on /force_load_new_markets
         fetch_website()
         # The next two lines are probably redundant.
         self.html = read_cached_html()
