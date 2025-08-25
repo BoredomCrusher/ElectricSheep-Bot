13eb833206959144d23c4bff7f9a8150a317513e
BoredomCrusher <Matrix1000atr9@gmail.com>
2025-08-25 14:58:44 -0700

    removing past leaderboards.

--- cogs/tracker.py
+++ cogs/tracker.py
@@ -205,13 +205,13 @@ class Tracker(commands.Cog):
     @tasks.loop(time=datetime.time(hour=0, minute=0, tzinfo=ZoneInfo("America/Los_Angeles")))
     async def delete_old_messages(self):
         channel = self.bot.get_channel(int(os.getenv("TRACKER_CHANNEL_ID")))
-        MAX_AGE = timedelta(days = 3)
+        MAX_AGE = timedelta(days = 2)
         
         async for message in channel.history(limit=100):
             now = datetime.datetime.now(ZoneInfo("America/Los_Angeles"))
             message_time = message.created_at.astimezone(ZoneInfo("America/Los_Angeles"))
             message_age = now - message_time
-            if message_age > MAX_AGE:
+            if message_age > MAX_AGE or ("Reading Streaks" in message and message_age >= 1):
                 try:
                     await message.delete()
                     print("old message deleted")
